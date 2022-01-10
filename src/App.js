65e25534252fa6cb2d8941148de86541519c24b0
Martin <mdsugarev@gmail.com>
2022-01-10 20:46:10 +0200

    e

--- src/App.js
+++ src/App.js
@@ -7,28 +7,28 @@ function App() {
 
  useEffect(() => {
    console.log(text);
-   
- }, [text])
+   const t = localStorage.getItem('savedData')
+   SetText(t)
+ }, [])
 
  const handleChange = (e) => {
    SetText(e.target.value)
  }
  const handleSave = () => {
    localStorage.setItem('savedData', text)
+   console.log(localStorage.getItem('savedData'))
  }
  const handleClear = () => {
    SetText("")
  }
- const handleLoad = () => {
-   SetText(localStorage.getItem('savedData'))
- }
+ 
 
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea onChange={handleChange} onLoad={handleLoad} value={text}  className="textarea is-large" placeholder="Notes..." />
+            <textarea onChange={handleChange}  value={text}  className="textarea is-large" placeholder="Notes..." />
           </div>
         </div>
         <button onClick={handleSave} className="button is-large is-primary is-active">Save</button>
